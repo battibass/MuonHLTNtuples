b4c7b0b0118f5cf79f44d92ae57e89d4e36acb95
Carlo Battilana <carlo.battilana@gmail.com>
2014-07-15 14:57:48 +0200

    New version, works with hltGetConfiguration cfgs :-)

--- CMSSWCiemat/Top2012/python/MuonHltNtuples_cff.py
+++ CMSSWCiemat/Top2012/python/MuonHltNtuples_cff.py
@@ -1,11 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 
-def appendMuonHltNtuple(process, runOnMC, ntupleFileName="MuonHltTree.root") :
+def appendMuonHltNtuple(process, runOnMC, processTag="HLT", ntupleFileName="MuonHltTree.root") :
 
     process.load("CMSSWCiemat.Top2012.MuonHltTreeProducer_cfi")
 
-    process.MuonHltTree.PileUpInfoTag = "none" # CB hack for now 
+    process.MuonHltTree.PileUpInfoTag = "none" # CB hack for now
+
+    if processTag != "HLT" :
+        print "[MuonHltNtuples]: Customising process tag for TriggerResults / Summary to :", processTag
+        process.MuonHltTree.TrigResultsTag = "TriggerResults::"+processTag
+        process.MuonHltTree.TrigSummaryTag = "hltTriggerSummaryAOD::"+processTag
 
     if runOnMC :
         process.load("CMSSWCiemat.Top2012.PrunedGenParticles_cfi")
@@ -17,9 +22,9 @@ def appendMuonHltNtuple(process, runOnMC, ntupleFileName="MuonHltTree.root") :
         fileName = cms.string(ntupleFileName)
     )
 
-    if hasattr(process,"out") :
-        print "[MuonHltNtuples]: EndPath out found, appending ntuples"
-        process.out.append(muonHltNtuple)
+    if hasattr(process,"AOutput") :
+        print "[MuonHltNtuples]: EndPath AOutput found, appending ntuples"
+        process.AOutput.replace(process.hltOutputA, process.hltOutputA + process.muonHltNtuple)
     else :
-        print "[MuonHltNtuples]: EndPath out not found, creating it for ntuple sequence"
-        process.out = cms.EndPath(process.muonHltNtuple)
+        print "[MuonHltNtuples]: EndPath AOuptput not found, creating it for ntuple sequence"
+        process.AOutput = cms.EndPath(process.muonHltNtuple)
