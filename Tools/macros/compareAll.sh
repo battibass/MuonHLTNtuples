fb96758681128272e76d6b011eead17de7f8d644
Carlo Battilana <carlo.battilana@gmail.com>
2014-08-08 11:18:51 +0200

    53X is now firs sample (reference for SFs)

--- Tools/macros/compareAll.sh
+++ Tools/macros/compareAll.sh
@@ -6,8 +6,8 @@ SAMPLES=('WToMuNu' 'QCDMu2030' 'QCDMu3050' 'QCDMu5080' 'QCDMu80120')
 
 for SAMPLE in ${SAMPLES[@]} ; do
     echo $SAMPLE
-    ./comparePlots.exe results/rateResults_${NTUPLE_VERSION}_62X_HLT701_13TeV_20PU_25ns_${SAMPLE}.root \
-	               results/rateResults_${NTUPLE_VERSION}_53X_HLT701_13TeV_25PU_25ns_${SAMPLE}.root \
-                       results/rateResults_${NTUPLE_VERSION}_53X_HLT701_8TeV_25PU_25ns_${SAMPLE}.root
+    ./comparePlots.exe results/rateResults_${NTUPLE_VERSION}_53X_HLT701_13TeV_25PU_25ns_${SAMPLE}.root \
+                       results/rateResults_${NTUPLE_VERSION}_62X_HLT701_13TeV_20PU_25ns_${SAMPLE}.root \
+	               results/rateResults_${NTUPLE_VERSION}_53X_HLT701_8TeV_25PU_25ns_${SAMPLE}.root
     mv results/comparePlots results/comparePlots_${SAMPLE}
 done
