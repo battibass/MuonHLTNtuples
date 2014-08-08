af2fcdc22732722f54f4572a9bfb6a6348d6e17d
Carlo Battilana <carlo.battilana@gmail.com>
2014-08-08 11:37:25 +0200

    run analysis on all samples and scales according to xsec and filter eff

---
+++ Tools/macros/hltRateAnalysisAll.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# WToMuNu
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_8TeV_25PU_25ns_WToMuNu.root     9130.0 1.0
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_13TeV_25PU_25ns_WToMuNu.root   16000.0 1.0
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_62X_HLT701_13TeV_20PU_25ns_WToMuNu.root   16100.0 1.0
+
+# QCDMu2030
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_8TeV_25PU_25ns_QCDMu2030.root        2.87E8 0.0065
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_13TeV_25PU_25ns_QCDMu2030.root    582500000 0.0051
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_62X_HLT701_13TeV_20PU_25ns_QCDMu2030.root    675300000 0.0065
+
+# QCDMu3050
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_8TeV_25PU_25ns_QCDMu3050.root       6.609E7 0.0122
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_13TeV_25PU_25ns_QCDMu3050.root    141700000 0.0126
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_62X_HLT701_13TeV_20PU_25ns_QCDMu3050.root    164400000 0.0122
+
+# QCDMu5080
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_8TeV_25PU_25ns_QCDMu5080.root       8082000 0.0218 
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_13TeV_25PU_25ns_QCDMu5080.root     19090000 0.0232
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_62X_HLT701_13TeV_20PU_25ns_QCDMu5080.root     21930000 0.0218
+
+# QCDMu80120
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_8TeV_25PU_25ns_QCDMu80120.root       1024000 0.0395 
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_53X_HLT701_13TeV_25PU_25ns_QCDMu80120.root      2670000 0.0360
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v7_62X_HLT701_13TeV_20PU_25ns_QCDMu80120.root      3000000 0.0395
+
+
