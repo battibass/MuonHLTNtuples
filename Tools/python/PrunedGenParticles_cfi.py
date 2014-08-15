b7c7de50f29849901e49812dba636bfef8d08d10
Carlo Battilana <carlo.battilana@gmail.com>
2014-08-15 11:34:53 +0200

    status definition in GEN particles changes from pythia 6 to pythia 8! only status 1 particles are expected to be the same! changing pruned GEN particles.
    this is production v10

--- Tools/python/PrunedGenParticles_cfi.py
+++ Tools/python/PrunedGenParticles_cfi.py
@@ -3,10 +3,7 @@ import FWCore.ParameterSet.Config as cms
 prunedGenParticles = cms.EDProducer("GenParticlePruner",
                                     src = cms.InputTag("genParticles"),
                                     select = cms.vstring("drop *"
-                                                         , "keep status = 3"
-                                                         , "++keep pdgId=11 & pt>10 & abs(eta)<3"
-                                                         , "++keep pdgId=13 & pt>10 & abs(eta)<3"
-                                                         , "++keep pdgId=-11 & pt>10 & abs(eta)<3"
-                                                         , "++keep pdgId=-13 & pt>10 & abs(eta)<3"
+                                                         , "keep pdgId =  13"
+                                                         , "keep pdgId = -13"
                                                          )
                                     )
