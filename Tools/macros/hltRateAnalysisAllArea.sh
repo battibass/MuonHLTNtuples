06719f865c724f2260b30af293f52b9fdd682876
Carlo Battilana <carlo.battilana@gmail.com>
2014-08-18 12:04:00 +0200

    Analysis scripts migrated to v10

---
+++ Tools/macros/hltRateAnalysisAllArea.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+VERSION="v10"
+
+# WToMuNu
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_WToMuNu.root      -1000 
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_WToMuNu.root     -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_WToMuNu.root     -1000
+
+# QCDMu2030
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu2030.root    -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu2030.root   -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu2030.root   -1000
+
+# QCDMu3050
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu3050.root    -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu3050.root   -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu3050.root   -1000
+
+# QCDMu5080
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu5080.root    -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu5080.root   -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu5080.root   -1000
+
+# QCDMu80120
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu80120.root   -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu80120.root  -1000
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu80120.root  -1000
+
+
---
+++ Tools/macros/hltRateAnalysisAllNoXSec.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+VERSION="v10"
+
+# WToMuNu
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_WToMuNu.root    
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_WToMuNu.root   
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_WToMuNu.root   
+
+# QCDMu2030
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu2030.root  
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu2030.root 
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu2030.root 
+
+# QCDMu3050
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu3050.root  
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu3050.root 
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu3050.root 
+
+# QCDMu5080
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu5080.root   
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu5080.root 
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu5080.root 
+
+# QCDMu80120
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_8TeV_25PU_25ns_QCDMu80120.root  
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_53X_HLT701_13TeV_25PU_25ns_QCDMu80120.root
+./hltRateAnalysis /data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_${VERSION}_62X_HLT701_13TeV_20PU_25ns_QCDMu80120.root
+
+
