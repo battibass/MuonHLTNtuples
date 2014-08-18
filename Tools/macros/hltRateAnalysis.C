7cdcbbdebb72a064544f41977e15fd8b54d60b7f
Carlo Battilana <carlo.battilana@gmail.com>
2014-08-18 12:10:47 +0200

    Better debugging of GEN particles (dump, multiplicity plots)

--- Tools/macros/hltRateAnalysis.C
+++ Tools/macros/hltRateAnalysis.C
@@ -21,16 +21,14 @@ using namespace ciemat;
 
 // BASE cuts and definitions
 // to configute the analysis
-const int   PT_THR       = 24;
-const int   MAX_ETA      = 2.4;
-const float ETA_BINS[13] = {-2.4, -2.1, -1.6, -1.2, -0.8, -0.4, 0., 0.4, 0.8, 1.2, 1.6, 2.1, 2.4};
+const float ETA_BINS[17] = {-5., -3., -2.4, -2.1, -1.6, -1.2, -0.8, -0.4, 0., 0.4, 0.8, 1.2, 1.6, 2.1, 2.4, 3., 5.};
 
 class RatePlotter 
 {
 
 public:
 
-  RatePlotter(float ptThr, float maxEta, std::string filterTag);
+  RatePlotter(float ptThr, float maxEta, std::string filterTag, bool controlPlots);
   ~RatePlotter();
 
   void book(TFile* fileOut);
@@ -43,15 +41,16 @@ private:
   float m_ptThr;
   float m_maxEta;
   std::string m_filterTag;
+  bool m_controlPlots;
 
   std::map<std::string, TH1F*> m_histoMap;
 
 };
 
-RatePlotter::RatePlotter(float ptThr, float maxEta, std::string filterTag) :
-                         m_ptThr(ptThr), m_maxEta(maxEta), m_filterTag(filterTag)
+RatePlotter::RatePlotter(float ptThr, float maxEta, std::string filterTag, bool controlPlots) :
+  m_ptThr(ptThr), m_maxEta(maxEta), m_filterTag(filterTag), m_controlPlots(controlPlots)
 { 
-
+  
 }
 
 RatePlotter::~RatePlotter()
@@ -80,8 +79,8 @@ void RatePlotter::book(TFile* fileOut)
   Float_t pi = TMath::Pi();
 
   // Declare histograms
-  m_histoMap["hHLTvsEta"] = new TH1F(("hHLTvsEta"+plotterTag).c_str(), ("HLT occupancy vs #eta " + plotterTag +";HLT mu #eta;A.U.").c_str(), 12, ETA_BINS);
-  m_histoMap["hGENvsEta"] = new TH1F(("hGENvsEta"+plotterTag).c_str(), ("GEN occupancy vs #eta " + plotterTag +";GEN mu #eta; A.U.").c_str(), 12, ETA_BINS);
+  m_histoMap["hHLTvsEta"] = new TH1F(("hHLTvsEta"+plotterTag).c_str(), ("HLT occupancy vs #eta " + plotterTag +";HLT mu #eta;A.U.").c_str(), 16, ETA_BINS);
+  m_histoMap["hGENvsEta"] = new TH1F(("hGENvsEta"+plotterTag).c_str(), ("GEN occupancy vs #eta " + plotterTag +";GEN mu #eta; A.U.").c_str(), 16, ETA_BINS);
 
   m_histoMap["hHLTvsPhi"] = new TH1F(("hHLTvsPhi"+plotterTag).c_str(), ("HLT occupancy vs #phi " + plotterTag +";HLT mu #phi [rad];A.U.").c_str(), 10, -pi, pi);
   m_histoMap["hGENvsPhi"] = new TH1F(("hGENvsPhi"+plotterTag).c_str(), ("GEN occupancy vs #phi " + plotterTag +";GEN mu #phi [rad]; A.U.").c_str(), 10, -pi, pi);
@@ -95,6 +94,18 @@ void RatePlotter::book(TFile* fileOut)
   m_histoMap["hHLTvsInter"] = new TH1F(("hHLTvsInter"+plotterTag).c_str(), ("HLT occupancy vs PU" + plotterTag +"; N GEN vtx; A.U.").c_str(), 35, 0., 70.);
   m_histoMap["hGENvsInter"] = new TH1F(("hGENvsInter"+plotterTag).c_str(), ("GEN occupancy vs PU" + plotterTag +"; N GEN vtx; A.U.").c_str(), 35, 0., 70.);
 
+  if (m_controlPlots)
+    {
+      m_histoMap["hGENMultip"] = new TH1F(("hGENMultip"+plotterTag).c_str(), ("GEN multiplicity of mu+ + mu- " + plotterTag +";# muons;A.U.").c_str(), 30, -0.5, 29.5);
+      m_histoMap["hGENMultip1"] = new TH1F(("hGENMultip1"+plotterTag).c_str(), ("GEN multiplicity of mu+ + mu- of status 1" + plotterTag +";# muons;A.U.").c_str(), 30, -0.5, 29.5);
+
+      m_histoMap["hGENvsEtaLowPt"] = new TH1F(("hGENvsEtaLowPt"+plotterTag).c_str(), ("GEN occupancy vs #eta p_{T} < 5 GeV " + plotterTag +";GEN mu #eta;A.U.").c_str(), 16, ETA_BINS);
+      m_histoMap["hGENvsPhiLowPt"] = new TH1F(("hGENvsPhiLowPt"+plotterTag).c_str(), ("GEN occupancy vs #phi p_{T} < 5 GeV " + plotterTag +";GEN mu #phi [rad]; A.U.").c_str(), 10, -pi, pi);
+
+      m_histoMap["hGENvsEtaAll"] = new TH1F(("hGENvsEtaAll"+plotterTag).c_str(), ("GEN occupancy vs #eta " + plotterTag +" all muons;GEN mu #eta; A.U.").c_str(), 16, ETA_BINS);
+      m_histoMap["hGENvsPhiAll"] = new TH1F(("hGENvsPhiAll"+plotterTag).c_str(), ("GEN occupancy vs #phi " + plotterTag +" all muons;GEN mu #phi [rad]; A.U.").c_str(), 10, -pi, pi);
+      m_histoMap["hGENvsPtAll"] = new TH1F(("hGENvsPtAll"+plotterTag).c_str(), ("GEN occupancy vs p_{T} " + plotterTag +" all muons;GEN mu p_{T} [GeV]; A.U.").c_str(), nPtBins, minPtBin, maxPtBin);
+    }
 
 }
 
@@ -106,8 +117,43 @@ void RatePlotter::fill(ciemat::Event *ev)
   std::vector<ciemat::GenParticle>::const_iterator genPartIt  = ev->genParticles.begin();
   std::vector<ciemat::GenParticle>::const_iterator genPartEnd = ev->genParticles.end();
       
+  // std::cout << "Here is one event:" << std::endl;
+
+  int nMu  = 0;
+  int nMu1 = 0;
+
   for (; genPartIt != genPartEnd; ++genPartIt) 
     {
+
+      if (m_controlPlots)
+	{
+
+	  if (abs(genPartIt->pdgId)==13)
+	    {
+	      nMu++;
+	      if (genPartIt->status==1)
+		{
+		  nMu1++;
+		  m_histoMap["hGENvsEtaAll"]->Fill(genPartIt->eta);
+		  m_histoMap["hGENvsPhiAll"]->Fill(genPartIt->phi);
+		  m_histoMap["hGENvsPtAll"]->Fill(genPartIt->pt);
+		  if (genPartIt->pt < 5.)
+		    {
+		      m_histoMap["hGENvsEtaLowPt"]->Fill(genPartIt->eta);
+		      m_histoMap["hGENvsPhiLowPt"]->Fill(genPartIt->phi);
+		    }
+		}
+	    }
+	}
+
+      // if (abs(genPartIt->pdgId)==13) 
+      // 	{
+      // 	  std::cout << genPartIt->pdgId  << " "
+      // 		    << genPartIt->status << " "
+      // 		    << genPartIt->pt     << " "
+      // 		    << genPartIt->eta    << std::endl;
+      // 	}
+
       if (genPartIt->status == 1           &&
 	  abs(genPartIt->pdgId)==13        &&
 	  fabs(genPartIt->eta) <= m_maxEta &&
@@ -124,6 +170,12 @@ void RatePlotter::fill(ciemat::Event *ev)
 	    }
 
 	}
+    }
+
+  if (m_controlPlots)
+    {
+      m_histoMap["hGENMultip"]->Fill(nMu);
+      m_histoMap["hGENMultip1"]->Fill(nMu1++);
     }
 
   if (bestGenPart) 
@@ -195,6 +247,14 @@ void RatePlotter::plotAndSave(float scaleFactor)
   for(; histoMapIt!=histoMapEnd; ++histoMapIt)
     {
       histoMapIt->second->Sumw2();
+
+      if (scaleFactor < 0. )
+	{
+	  // CB hack 4 simone if SF < 0
+	  // normalise by Integral o plot
+	  float area = histoMapIt->second->Integral();
+	  scaleFactor = area>1 ? 1./area : 1;
+	}
       histoMapIt->second->Scale(scaleFactor);
     }
 
@@ -233,7 +293,7 @@ int main(int argc, char* argv[]){
     }
   
   // Input root file
-  TString fileName = argv[1]; // "/data1/battilan/MuonHLT/RobertoRateChecks/MuonHltTree_v5_53X_HLT701_25PU_25ns_QCDMu3050.root";
+  TString fileName = argv[1];
 
   int tagBegin = fileName.Index("MuonHltTree")+12;
   int tagEnd   = fileName.Index(".root");
@@ -246,20 +306,23 @@ int main(int argc, char* argv[]){
   TRint* app = new TRint("CMS Root Application", &argc, argv);
 
   setTDRStyle();
-
+  
   std::vector<RatePlotter> plotters;
 
-  plotters.push_back(RatePlotter(16,2.4,"hltL1sMu16"));
-  plotters.push_back(RatePlotter(16,2.4,"hltL2fL1sMu16L1f0L2Filtered16Q"));
-  plotters.push_back(RatePlotter(24,2.4,"hltL3fL1sMu16L1f0L2f16QL3Filtered24Q"));
-  plotters.push_back(RatePlotter(40,2.4,"hltL3fL1sMu16L1f0L2f16QL3Filtered40Q"));
-  plotters.push_back(RatePlotter(24,2.4,"hltL3crIsoL1sMu16L1f0L2f16QL3f24QL3crIsoRhoFiltered0p15"));
+  plotters.push_back(RatePlotter(0.,5.,"genReferencePt0",true));
+  plotters.push_back(RatePlotter(1.,3.,"genReferencePt11",false));
+
+  plotters.push_back(RatePlotter(16,2.4,"hltL1sMu16",false));
+  plotters.push_back(RatePlotter(16,2.4,"hltL2fL1sMu16L1f0L2Filtered16Q",false));
+  plotters.push_back(RatePlotter(24,2.4,"hltL3fL1sMu16L1f0L2f16QL3Filtered24Q",false));
+  plotters.push_back(RatePlotter(40,2.4,"hltL3fL1sMu16L1f0L2f16QL3Filtered40Q",false));
+  plotters.push_back(RatePlotter(24,2.4,"hltL3crIsoL1sMu16L1f0L2f16QL3f24QL3crIsoRhoFiltered0p15",false));
 	   
-  plotters.push_back(RatePlotter(16,2.1,"hltL1sMu16"));
-  plotters.push_back(RatePlotter(16,2.1,"hltL2fL1sMu16L1f0L2Filtered16Q"));
-  plotters.push_back(RatePlotter(24,2.1,"hltL3fL1sMu16L1f0L2f16QL3Filtered24Q"));
-  plotters.push_back(RatePlotter(40,2.1,"hltL3fL1sMu16L1f0L2f16QL3Filtered40Q"));
-  plotters.push_back(RatePlotter(24,2.1,"hltL3crIsoL1sMu16L1f0L2f16QL3f24QL3crIsoRhoFiltered0p15"));
+  plotters.push_back(RatePlotter(16,2.1,"hltL1sMu16",false));
+  plotters.push_back(RatePlotter(16,2.1,"hltL2fL1sMu16L1f0L2Filtered16Q",false));
+  plotters.push_back(RatePlotter(24,2.1,"hltL3fL1sMu16L1f0L2f16QL3Filtered24Q",false));
+  plotters.push_back(RatePlotter(40,2.1,"hltL3fL1sMu16L1f0L2f16QL3Filtered40Q",false));
+  plotters.push_back(RatePlotter(24,2.1,"hltL3crIsoL1sMu16L1f0L2f16QL3f24QL3crIsoRhoFiltered0p15",false));
 
   // Initialize pointers to summary and full event structure
  
@@ -321,21 +384,7 @@ int main(int argc, char* argv[]){
       plotterIt->plotAndSave(scaleFactor);
     }
   
-  outputFile->Write();  
-
-  // TCanvas * cGen = new TCanvas("GenVsEta","GenVsEta",500,500);
-
-  // hGENvsEta->Scale(1./nEntries);
-  // hGENvsEta->Draw();
-
-  // cGen->SaveAs("genVsEta_"+fileTag+".C");
-
-  // TCanvas * cL3 = new TCanvas("L3VsPt","L3VsPt",500,500);
-
-  // hL3vsEta->Scale(1./nEntries);
-  // hL3vsEta->Draw();
-
-  // cL3->SaveAs("hltVsEta_"+fileTag+".C");
+  outputFile->Write();
 
   if (!gROOT->IsBatch()) app->Run();
 
