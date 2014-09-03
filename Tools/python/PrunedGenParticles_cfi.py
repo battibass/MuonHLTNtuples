f8d67869e9532dffd92db4f5f68d565979be7533
Carlo Battilana <carlo.battilana@gmail.com>
2014-09-03 15:19:58 +0200

    Added mothers in pruning to check pions and kaons

--- Tools/python/PrunedGenParticles_cfi.py
+++ Tools/python/PrunedGenParticles_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 prunedGenParticles = cms.EDProducer("GenParticlePruner",
                                     src = cms.InputTag("genParticles"),
                                     select = cms.vstring("drop *"
-                                                         , "keep pdgId =  13"
-                                                         , "keep pdgId = -13"
+                                                         , "++keep pdgId =  13"
+                                                         , "++keep pdgId = -13"
                                                          )
                                     )
